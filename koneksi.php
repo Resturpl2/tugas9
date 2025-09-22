ec7b676cee7024f569a904b659b7eddf1df10a21
Resturpl2 <anandarestu214@gmail.com>
2025-09-22 15:55:44 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,16 @@
 <?php
-$host     = "localhost";
-$user     = "xirpl2-3";
-$pass     = "3091100981";
-$db       = "db_xirpl2-3_1";
-$conn = mysqli_connect($host, $user, $pass, $db);
+$host = "localhost";
+$user = "xirpl2-3";
+$pass = "3091100981";
+$db   = "db_xirpl2-3_1"; // database dari guru
 
+$conn = mysqli_connect($host, $user, $pass);
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
-} else {
-    echo "Koneksi berhasil!";
+}
+
+// Pilih database (karena ada tanda minus -)
+if (!mysqli_query($conn, "USE `$db`")) {
+    die("Tidak bisa memilih database: " . mysqli_error($conn));
 }
 ?>
