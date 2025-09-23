9b8a90466aaa38920754936e07d1c30e5923073c
Resturpl2 <anandarestu214@gmail.com>
2025-09-23 12:26:14 +0700

    Update tambah.php

--- tambah.php
+++ tambah.php
@@ -5,8 +5,11 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $spek = $_POST['spek'];
     $harga = $_POST['harga'];
     $stok = $_POST['stok'];
+     $kode_otp = $_POST['kode_otp'];
+     $status = $_POST['status'];
 
-    $query = "INSERT INTO akun_ff (spek, harga, stok) VALUES ('$spek', '$harga', '$stok')";
+    $query = "INSERT INTO akun_ff (spek, nickname, tier, harga, stok) 
+              VALUES ('$spek', '$nickname', '$tier', '$harga', '$stok')";
     if (mysqli_query($conn, $query)) {
         header("Location: index.php");
         exit;
@@ -26,6 +29,8 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         <p>Spek: <input type="text" name="spek" required></p>
         <p>Harga: <input type="number" name="harga" required></p>
         <p>Stok: <input type="number" name="stok" required></p>
+        <p>Kode_otp: <input type="text" name="kode_otp" required></p>
+        <p>Status: <input type="text" name="status" required></p>
         <button type="submit">Simpan</button>
     </form>
     <br>
